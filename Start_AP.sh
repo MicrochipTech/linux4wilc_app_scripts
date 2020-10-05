7463f36f211d3d04d64c90cefae5a4169796ad33
C21415 <hemanth.tenkod@microchip.com>
2020-10-05 01:36:31 -0700

    Adding error handling

--- Start_AP.sh
+++ Start_AP.sh
@@ -17,11 +17,11 @@ echo " #     # #          #     # #      #    # #    #   "
 echo " #     # #          ######  ###### #    #  ####    "
 echo "---------------------------------------------------"
 
-if ! lsmod | grep -q "wilc_sdio" ; then
+if lsmod | grep -q "wilc_sdio" ; then
 	echo "1. WILC-SDIO module is available"
 else
 	echo "1. Inserting the wilc-sdio module" 
-	modprobe wilc-sdio > wilc-sdio_module.log
+	modprobe wilc-sdio >> wilc-sdio_module.log
 	if grep -q "Driver Initializing success" wilc-sdio_module.log ; then
 		echo "WILC-SDIO module insterted successfully"
 	else
@@ -30,28 +30,49 @@ else
 fi
 
 echo "2. Bringing up the wlan0 interface" 
-ifconfig wlan0 up > wlan0_up.log
-if ! ifconfig | grep -q "wlan0" ; then
+ifconfig wlan0 up >> wlan0_up.log
+if ifconfig | grep -q "wlan0" ; then
 	echo "Wireless LAN interface is UP!"
 else
 	echo "Wireless LAN interface has FAILED, check wlan0_up.log file"
 fi
 
 echo "3. Starting the Host AP deamon" 
-hostapd /etc/wilc_hostapd_open.conf -B &
-if ! ps | grep -q "hostapd" then
+hostapd /etc/wilc_hostapd_open.conf -B & >> hostapd.log
+if ps | grep -q "hostapd" ; 
+then
 	echo "hostapd process has started successfully"
 else
 	echo "hostapd has failed to start"
 fi
 
+if ! grep -q "tecmint.lan" /etc/dhcp/dhcpd.conf ; then
+cat << 'EOT' > /etc/dhcp/dhcpd.conf
+option domain-name "tecmint.lan";
+option domain-name-servers ns1.tecmint.lan, ns2.tecmint.lan;
+default-lease-time 3600;
+max-lease-time 7200;
+authoritative;
+
+subnet 192.168.0.1 netmask 255.255.255.0 {
+        option routers                  192.168.0.1;
+        option subnet-mask              255.255.0.0;
+        option domain-search            "tecmint.lan";
+        option domain-name-servers      192.168.0.1;
+        range   192.168.0.10   192.168.0.100;
+        range   192.168.0.110   192.168.0.200;
+}
+EOT
+fi
+
 echo "4. Configuring the AP IP Address to 192.168.0.1"
-ifconfig wlan0 192.168.0.1
+ifconfig wlan0 192.168.0.1 
 echo "5. Starting the DHCP server"
-/etc/init.d/S80dhcp-server start &
+#/etc/init.d/dhcp-server start &
+dhcpd -cf /etc/dhcp/dhcpd.conf 
 echo "6. Starting the WEB scoket deamon"
 cd /root
-./websocket &
+./websocket & 
 echo "Now, The device comes up as an Access Point(AP) and host a webpage to provision"
 echo "WiFi station interface"
 echo "\n"
